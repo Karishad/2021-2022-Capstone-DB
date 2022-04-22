a66561975999da8080e0bb1cf977044af816cca5
Olivia Gearner <olivia.gearner@gmail.com>
2022-04-22 17:53:50 -0500

    removed unused import useCallback from Find page

--- src/pages/FindPage.js
+++ src/pages/FindPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import axios from '../axios';
